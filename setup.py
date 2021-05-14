2b68d4d27434e664129d7ce70575905e9fa2003f
Poruri Sai Rahul <rporuri@enthought.com>
2021-05-14 21:10:55 +0530

    FEAT : Empty python package with a setup script
    the basics we can build on top of
    
    	new file:   .gitignore
    	new file:   enworkspaces/__init__.py
    	new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+# (C) Copyright 2021 Enthought, Inc., Austin, TX
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in LICENSE.txt and may be redistributed only under
+# the conditions described in the aforementioned license. The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+# Thanks for using Enthought open source!
+
+from setuptools import setup, find_packages
+
+if __name__ == "__main__":
+    __version__ = "0.0.0"
+
+    setup(
+        name="enworkspaces",
+        version=__version__,
+        license="BSD",
+        packages=find_packages(),
+        platforms=["Windows", "Linux", "Mac OS-X"],
+        python_requires=">=3.6",
+        zip_safe=True,
+    )
